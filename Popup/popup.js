5e3958e0ad537593a7b7df6b7d13f5cca02e19ef
Aaron <aaron.almeida5@gmail.com>
2021-02-20 17:23:35 -0500

    added exchanges

--- Popup/popup.js
+++ Popup/popup.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", documentEvents, false);
 
 async function myAction(input) {
+    let ticker = input.value.toUpperCase();
     let chosenSites = await getChosenSites();
+    let tickerMap = await getTickerMap();
+    let exchange = tickerMap[ticker];
+
     for (var name in chosenSites) {
-        site = chosenSites[name] + input.value;
+        Url = chosenSites[name]
+            .replace("{symbol}", ticker)
+            .replace("{exchange}", exchange);
         chrome.tabs.create({
-            url: site,
+            url: Url,
         });
     }
 }
 
-function documentEvents() {
+async function documentEvents() {
     const input = document.querySelector("#ticker");
     input.addEventListener("keyup", function (e) {
         if (e.keyCode === 13) {
@@ -26,6 +32,18 @@ function getChosenSites() {
         chrome.storage.local.get("chosenSites", function (result) {
             if (Object.values(result) != undefined) {
                 resolve(result.chosenSites);
+            } else {
+                reject();
+            }
+        });
+    });
+}
+
+function getTickerMap() {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get("tickerMap", function (result) {
+            if (Object.values(result) != undefined) {
+                resolve(result.tickerMap);
             } else {
                 reject();
             }
--- background.js
+++ background.js
@@ -6,8 +6,10 @@ chrome.runtime.onInstalled.addListener((details) => {
     console.log("Previous Version: ${previousVersion }");
     console.log("Current Version: ${currentVersion }");
     var map = {};
-    map["Finviz"] = "https://finviz.com/quote.ashx?t=";
-    map["Yahoo"] = "https://finance.yahoo.com/quote/";
+    map["Finviz"] = "https://finviz.com/quote.ashx?t={symbol}";
+    map["Yahoo"] = "https://finance.yahoo.com/quote/{symbol}";
+    map["TradingView"] = "https://www.tradingview.com/symbols/{symbol}";
+    map["MarketWatch"] = "https://www.marketwatch.com/investing/stock/{symbol}";
 
     switch (reason) {
         case "install":
@@ -16,4 +18,58 @@ chrome.runtime.onInstalled.addListener((details) => {
             chrome.storage.local.set({ chosenSites: map }, function () {});
             break;
     }
+
+    fetchExchangeTickers("TSX").then(function (response) {
+        json = response.reduce((json, value, key) => {
+            json[value] = "TSX";
+            return json;
+        }, {});
+
+        fetchExchangeTickers("NYSE").then(function (response2) {
+            json2 = response2.reduce((json2, value, key) => {
+                json2[value] = "NYSE";
+                return json2;
+            }, {});
+
+            fetchExchangeTickers("AMEX").then(function (response3) {
+                json3 = response3.reduce((json3, value, key) => {
+                    json3[value] = "AMEX";
+                    return json3;
+                }, {});
+
+                fetchExchangeTickers("NASDAQ").then(function (response4) {
+                    json4 = response4.reduce((json4, value, key) => {
+                        json4[value] = "NASDAQ";
+                        return json4;
+                    }, {});
+
+                    const result = {
+                        ...json,
+                        ...json2,
+                        ...json3,
+                        ...json4,
+                    };
+                    setTickerMap(result);
+                });
+            });
+        });
+    });
 });
+
+function setTickerMap(obj) {
+    chrome.storage.local.set({ tickerMap: obj }, function () {});
+}
+
+async function fetchExchangeTickers(exchange) {
+    return new Promise((resolve, reject) =>
+        fetch(
+            "https://dumbstockapi.com/stock?format=tickers-only&exchange=" +
+                exchange
+        )
+            .then((response) => response.json())
+            .then((responseData) => {
+                return resolve(responseData);
+            })
+            .catch((error) => console.warn(error))
+    );
+}
